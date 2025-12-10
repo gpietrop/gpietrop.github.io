42a6b4d14507c1d61af06bab011fa3921a43f0fa
gpietrop <67261061+gpietrop@users.noreply.github.com>
2025-12-10 13:12:43 +0000

    Deploying to gh-pages from @ gpietrop/gpietrop.github.io@27afda6858547c72b88d6541b83d55ef7c49ad5e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gpietrop/gloria-pietropolli.github.io",
+    "data-repo": "gpietrop/gpietrop.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
