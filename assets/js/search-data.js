58f478bb8e24c70cb3bbcb0355326caddbf7e5f2
gpietrop <67261061+gpietrop@users.noreply.github.com>
2025-12-10 13:56:58 +0000

    Deploying to gh-pages from @ gpietrop/gpietrop.github.io@b107b401fead218bae0a1123d049f14250b9d286 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%6C%6F%72%69%61.%70%69%65%74%72%6F%70%6F%6C%6C%69@%75%6E%69%74%73.%69%74", "_blank");
         },
       },{
         id: 'social-inspire',
