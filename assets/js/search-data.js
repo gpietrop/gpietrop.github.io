6064eb6e87bb4f19f6cc2f056f93e686bdebd2ec
gpietrop <67261061+gpietrop@users.noreply.github.com>
2025-12-11 14:35:39 +0000

    Deploying to gh-pages from @ gpietrop/gpietrop.github.io@a8b4009349ef074ea0d5025b4502e0fd789818bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -155,13 +155,6 @@ ninja.data = [{
           window.open("https://github.com/gpietrop", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
@@ -169,25 +162,11 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/gpietrop", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=-iUZFA4AAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
