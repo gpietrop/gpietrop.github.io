1489138ff1f7e673e5d873f04f08795501168162
gpietrop <67261061+gpietrop@users.noreply.github.com>
2026-04-08 19:17:52 +0000

    Deploying to gh-pages from @ gpietrop/gpietrop.github.io@979d28d3cf33b560dc0bfe2528f39cc8d91afc00 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
